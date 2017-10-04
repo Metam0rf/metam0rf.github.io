455233d5a37d08ea14ffd4340f4016fdfdc81eb5
Eugene Zozulya <zozulyaeugene@gmail.com>
2017-10-05 00:49:13 +0300

    JS 2 (edited)

--- JS_Module_2/js/main.js
+++ JS_Module_2/js/main.js
@@ -1,2 +1,12 @@
 "use strict";
 
+var today = new Date();;
+
+var userYearOfBirthday = document.getElementById("yearOfBirthday").value;
+
+if(userYearOfBirthday) {//Если заполнено - должно быть true
+    var years = dateNow.
+    if(userYearOfBirthday.length = 2){
+
+    }
+}

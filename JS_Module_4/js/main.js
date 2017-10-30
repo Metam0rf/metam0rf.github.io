b9f88f6f9df5faac2bc47ad0ad51c0be203a6686
Zozulia Evgenii <zozulyaeugene@gmail.com>
2017-10-30 18:30:32 +0200

    JS Module_4 (ver 0.4)

--- JS_Module_4/js/main.js
+++ JS_Module_4/js/main.js
@@ -14,8 +14,20 @@ var clickCounter = 0;
 
     var clickCountingElements = document.querySelectorAll(".clickCounting");
     clickCountingElements.forEach(function (item) {
-        item.addEventListener("click", clickCounting)
+        item.addEventListener("click", clickCounting);
     })
+
+    var powBttn = document.querySelector("#powBttn");
+    powBttn.addEventListener("click", powEvent);
+
+    var setBorderBottomBttn = document.querySelector("#setBorderBottomBttn");
+    setBorderBottomBttn.addEventListener("click", setBorderBottomEvent);
+
+    var ageInputElement = document.querySelector("#ageInput");
+    ageInputElement.addEventListener("change", onChangeAge);
+
+    var displayArrayLengthBttn = document.querySelector("#displayArrayLengthBttn");
+    displayArrayLengthBttn.addEventListener("click", clickArrayLengthBttnHandler);
 })();
 
 //---------Task 1
@@ -54,4 +66,73 @@ function clickCounting() {
     clickCounter += 1;
     var displayCounterText = document.getElementById("displayCounter");
     displayCounterText.textContent = "Количество нажатий: " + clickCounter;
+}
+
+//---------Task 5
+function powEvent() {
+    var numberElement = document.querySelector("#number");
+    var powNumberElement = document.querySelector("#powNumber");
+    var number = parseInt(numberElement.value);
+    var powNumber = parseInt(powNumberElement.value);
+    pow(number, powNumber);
+}
+
+function pow(number, pow) {
+    alert(Math.pow(number, pow));
+}
+
+//---------Task 6
+function setBorderBottomEvent() {
+    var tagName = document.querySelector("#tagNameInput").value;
+    setBorderBottomToElement(tagName);
+}
+
+function setBorderBottomToElement(elementName) {
+    elementName = elementName || "p";
+    var allElements = document.querySelectorAll(elementName);
+    allElements.forEach(function (item) {
+        item.style.setProperty("border-bottom","2px solid red","");
+    });
+}
+
+//---------Task 7
+function onChangeAge(event) {
+    var age = parseInt(event.target.value);
+    checkAge(age);
+}
+
+function checkAge(age) {
+
+    // Вариант с '||' когда age = 0
+    if(age || undefined === undefined){
+        alert("Введите возраст");
+        return;
+    }
+
+    //Вариант с 'age = undefined'
+    if(age === undefined){
+        alert("Введите возраст");
+        return;
+    }
+
+    age >= 16 ? alert("Добро пожаловать!") : alert("Вы еще молоды!");
+}
+
+//---------Task 9
+function clickArrayLengthBttnHandler() {
+    var arrayInput = document.querySelector("#arrayInput");
+    array = arrayInput.value.split(",");
+    alert(arrayInput.value);
+    var arrayLength = getArrayLength();
+    alert(arrayLength);
+}
+
+function getArrayLength(array) {
+
+    if(array === undefined){
+        alert("Ошибка рпределения длинны масива!");
+        return 0;
+    }
+
+    return array.length;
 }

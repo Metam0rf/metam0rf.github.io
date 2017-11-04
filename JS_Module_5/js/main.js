9728aa5ac68422bb104d833097090a96bea59f38
Zozulia Evgenii <zozulyaeugene@gmail.com>
2017-11-04 21:36:56 +0200

    JS Module_5 (ver 0.2)

--- JS_Module_5/js/main.js
+++ JS_Module_5/js/main.js
@@ -2,7 +2,18 @@
 
 //---------Task 1
 const textInputTask1 = document.querySelector("#userText");
-textInputTask1.addEventListener("change", function (e) {
+textInputTask1.addEventListener("change", function () {
     const textDisplayElement = document.querySelector("#displayText1");
     textDisplayElement.textContent = textInputTask1.value.length;
+});
+
+//---------Task 2
+const arrayOfImgPathes = ["adventure__340", "african-lion", "chestnut", "pumpkin"];
+const displayImagesBttn = document.querySelector("#displayImagesBttn");
+displayImagesBttn.addEventListener("click", function (e) {
+    for(let i=0; i<arrayOfImgPathes.length; i++){
+        let newImg = document.createElement("img");
+        newImg.setAttribute("src","assets/img/" + arrayOfImgPathes[i] + ".jpg");
+        displayImagesBttn.parentNode.appendChild(newImg);
+    }
 });

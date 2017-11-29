0c746fcf2ad8956b86a65b7c7f8234e094d195f3
Zozulia Evgenii <zozulyaeugene@gmail.com>
2017-11-29 17:55:22 +0200

    JS Module_6 (ver 1)

--- JS_Module_6/js/main.js
+++ JS_Module_6/js/main.js
@@ -46,3 +46,57 @@ const NUMBER_OF_DIVES = 307;
     }
     parentDiv.append(...DOMArray);
 })();
+
+//---------Task 4
+const setBackgroundToElement = document.querySelector(".gallery");
+const eventElement = document.querySelector("#task4");
+eventElement.addEventListener("click", (e) => {
+    if(e.target.classList.contains("gallery--img")){
+        setBackgroundToElement.style.backgroundImage = `url('${e.target.getAttribute("src")}')`;
+        setBackgroundToElement.style.backgroundSize = "conain";
+        setBackgroundToElement.style.backgroundRepeat = "no-repeat";
+    }
+});
+
+//---------Task 5
+const squareToMoveElement = document.querySelector("#squareToMove");
+let moveBottomCounter = 1;
+const DISTANCE_TO_MOVE = 100;
+const animatePositionY = (element , distance) => {
+    requestAnimationFrame(() => {
+        element.style.transform = `translateY(${distance}px)`;
+        moveBottomCounter += 1;
+    })
+};
+
+squareToMoveElement.addEventListener("click", () => {
+    animatePositionY(squareToMoveElement, DISTANCE_TO_MOVE * moveBottomCounter);
+});
+
+//---------Task 6
+const STATUS_ARRAY = [0,1,2];
+let currentStatus = 0;
+const toggleElement = document.querySelector(".toggleButton");
+const toggleBackground = document.querySelector(".toggleBackground");
+const parentRect = toggleBackground.getBoundingClientRect();
+
+const animatePositionX = (element , distance) => {
+    requestAnimationFrame(() => {
+        element.style.transform = `translateX(${distance}px)`;
+        moveBottomCounter += 1;
+    })
+};
+
+const setNextStatus = (e) => {
+    currentStatus = (currentStatus + 1) === STATUS_ARRAY.length ? 0 : currentStatus + 1;
+    let moveDistance = 0;
+    if(currentStatus === 0){
+        moveDistance = 0;
+    } else {
+        moveDistance = parentRect.width / 3 * currentStatus;
+    }
+    animatePositionX(e.target,moveDistance );
+    console.log(currentStatus);
+};
+
+toggleElement.addEventListener("click", setNextStatus);
